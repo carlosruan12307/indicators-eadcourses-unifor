afeb58013ecfc6c234dccb836a65d8038a4b0908
ThaisRozas <thaisrozas@gmail.com>
2024-10-01 13:39:09 -0300

    feature: selectbox with placeholder and pie chart without shadow

--- pages/Ambientes.py
+++ pages/Ambientes.py
@@ -25,88 +25,89 @@ st.header("Secretaria Virtual")
 # Adicionar o filtro de data acima do gráfico
 st.subheader("Filtrar por Data")
 
-# Seletor de período
+# Seletor de período com um placeholder
 date_filter_option = st.selectbox(
     "Selecione o período:",
-    ("Data específica", "Última semana", "Últimas duas semanas", "Último mês")
+    ["Escolha o período", "Data específica", "Última semana", "Últimas duas semanas", "Último mês"]
 )
 
-# Escolha da data caso "Data específica" seja selecionada
-if date_filter_option == "Data específica":
-    selected_date = st.date_input("Escolha uma data", datetime.today())
-
-# Botão para gerar o relatório
-generate_report = st.button("Gerar Relatório")
-
-# Apenas gera o relatório se o botão for pressionado
-if generate_report:
-    # Filtrar os dados com base na seleção do usuário
+# Verificar se o usuário selecionou um período válido
+if date_filter_option != "Escolha o período":
+    # Escolha da data caso "Data específica" seja selecionada
     if date_filter_option == "Data específica":
-        # Filtrar os dados apenas para a data selecionada
-        filtered_data = data[data['Data'] == pd.to_datetime(selected_date)]
-    elif date_filter_option == "Última semana":
-        one_week_ago = datetime.today() - timedelta(weeks=1)
-        filtered_data = data[data['Data'] >= one_week_ago]
-    elif date_filter_option == "Últimas duas semanas":
-        two_weeks_ago = datetime.today() - timedelta(weeks=2)
-        filtered_data = data[data['Data'] >= two_weeks_ago]
-    elif date_filter_option == "Último mês":
-        one_month_ago = datetime.today() - timedelta(days=30)
-        filtered_data = data[data['Data'] >= one_month_ago]
+        selected_date = st.date_input("Escolha uma data", datetime.today())
 
-    # Calcular a quantidade total de acessos
-    total_accesses = len(filtered_data)
+    # Botão para gerar o relatório
+    generate_report = st.button("Gerar Relatório")
 
-    # Calcular a divisão de acessos por cada tipo de página
-    access_counts_per_page = filtered_data['Contexto do Evento'].value_counts()
+    # Apenas gera o relatório se o botão for pressionado
+    if generate_report:
+        # Filtrar os dados com base na seleção do usuário
+        if date_filter_option == "Data específica":
+            # Filtrar os dados apenas para a data selecionada
+            filtered_data = data[data['Data'] == pd.to_datetime(selected_date)]
+        elif date_filter_option == "Última semana":
+            one_week_ago = datetime.today() - timedelta(weeks=1)
+            filtered_data = data[data['Data'] >= one_week_ago]
+        elif date_filter_option == "Últimas duas semanas":
+            two_weeks_ago = datetime.today() - timedelta(weeks=2)
+            filtered_data = data[data['Data'] >= two_weeks_ago]
+        elif date_filter_option == "Último mês":
+            one_month_ago = datetime.today() - timedelta(days=30)
+            filtered_data = data[data['Data'] >= one_month_ago]
 
-    # Calcular a quantidade total de estudantes únicos
-    unique_students = filtered_data['Nome completo'].nunique()
+        # Calcular a quantidade total de acessos
+        total_accesses = len(filtered_data)
 
-    # Exibir os resultados
-    st.subheader("Resultados do Período Selecionado")
-    st.write(f"**Total de acessos:** {total_accesses}")
-    st.write(f"**Quantidade total de estudantes únicos:** {unique_students}")
+        # Calcular a divisão de acessos por cada tipo de página
+        access_counts_per_page = filtered_data['Contexto do Evento'].value_counts()
 
-    st.subheader("Divisão de Acessos por Página")
-    for page, count in access_counts_per_page.items():
-        cleaned_page = page.replace('Página: ', '').replace('Disciplina: ', '')
-        st.write(f"**{cleaned_page}**: {count} acessos")
+        # Calcular a quantidade total de estudantes únicos
+        unique_students = filtered_data['Nome completo'].nunique()
 
-# Análise adicional de estudantes únicos
-    # Filtrar estudantes que acessaram a "Disciplina: Secretaria Virtual EaD"
-    students_accessing_discipline = filtered_data[filtered_data['Contexto do Evento'] == 'Disciplina: Secretaria Virtual EaD']['Nome completo'].unique()
+        # Exibir os resultados
+        st.subheader("Resultados do Período Selecionado")
+        st.write(f"**Total de acessos:** {total_accesses}")
+        st.write(f"**Quantidade total de estudantes únicos:** {unique_students}")
 
-    # Filtrar estudantes que acessaram qualquer página (excluindo a disciplina)
-    students_accessing_pages = filtered_data[
-        filtered_data['Contexto do Evento'].str.contains('Página: ')
-    ]['Nome completo'].unique()
+        st.subheader("Divisão de Acessos por Página")
+        for page, count in access_counts_per_page.items():
+            cleaned_page = page.replace('Página: ', '').replace('Disciplina: ', '')
+            st.write(f"**{cleaned_page}**: {count} acessos")
 
-    # Estudantes que acessaram a disciplina e também acessaram alguma página
-    students_discipline_and_pages = set(students_accessing_discipline).intersection(set(students_accessing_pages))
+        # Análise adicional de estudantes únicos
+        students_accessing_discipline = filtered_data[filtered_data['Contexto do Evento'] == 'Disciplina: Secretaria Virtual EaD']['Nome completo'].unique()
+        students_accessing_pages = filtered_data[
+            filtered_data['Contexto do Evento'].str.contains('Página: ')
+        ]['Nome completo'].unique()
 
-    # Estudantes que acessaram apenas a disciplina sem acessar as páginas
-    students_only_discipline = set(students_accessing_discipline).difference(students_accessing_pages)
+        # Estudantes que acessaram a disciplina e também acessaram alguma página
+        students_discipline_and_pages = set(students_accessing_discipline).intersection(set(students_accessing_pages))
 
-    # Exibir os resultados da análise de estudantes únicos
-    st.subheader("Análise")
-    st.write(f"**Total de estudantes que acessaram a disciplina (Secretaria Virtual EaD):** {len(students_accessing_discipline)}")
-    st.write(f"**Total de estudantes que acessaram a disciplina e também acessaram as páginas:** {len(students_discipline_and_pages)}")
-    st.write(f"**Total de estudantes que acessaram apenas a disciplina sem entrar nas páginas:** {len(students_only_discipline)}")
+        # Estudantes que acessaram apenas a disciplina sem acessar as páginas
+        students_only_discipline = set(students_accessing_discipline).difference(students_accessing_pages)
 
-    # Dados para o gráfico de pizza
-    labels = [
-        'Disciplina e Páginas',
-        'Apenas a Disciplina'
-    ]
-    sizes = [len(students_discipline_and_pages), len(students_only_discipline)]
-    colors = ['#66b3ff', '#99ff99']
-    explode = (0.1, 0)  # Destaque para o primeiro segmento
+        # Exibir os resultados da análise de estudantes únicos
+        st.subheader("Análise")
+        st.write(f"**Total de estudantes que acessaram a disciplina (Secretaria Virtual EaD):** {len(students_accessing_discipline)}")
+        st.write(f"**Total de estudantes que acessaram a disciplina e também acessaram as páginas:** {len(students_discipline_and_pages)}")
+        st.write(f"**Total de estudantes que acessaram apenas a disciplina sem entrar nas páginas:** {len(students_only_discipline)}")
 
-    # Criar o gráfico de pizza
-    fig, ax = plt.subplots(figsize=(8, 6))
-    ax.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', startangle=90, shadow=True)
-    ax.axis('equal')  # Equaliza o gráfico de pizza
+        # Dados para o gráfico de pizza
+        labels = [
+            'Disciplina e Páginas',
+            'Apenas a Disciplina'
+        ]
+        sizes = [len(students_discipline_and_pages), len(students_only_discipline)]
+        colors = ['#66b3ff', '#99ff99']
+        explode = (0.1, 0)  # Destaque para o primeiro segmento
 
-    # Exibir o gráfico de pizza no Streamlit
-    st.pyplot(fig)
+        # Criar o gráfico de pizza
+        fig, ax = plt.subplots(figsize=(8, 6))
+        ax.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', startangle=90)
+        ax.axis('equal')  # Equaliza o gráfico de pizza
+
+        # Exibir o gráfico de pizza no Streamlit
+        st.pyplot(fig)
+else:
+    st.info("Por favor, selecione um período para gerar o relatório.")
